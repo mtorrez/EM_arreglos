555e8ef887fa0d275160b2238a1bd5940b72b9eb
Mario Torrez <mtorrezc@gmail.com>
2016-05-07 09:38:04 -0400

    Segundo commit

--- index.php
+++ index.php
@@ -9,6 +9,11 @@
 	<p>Los arreglos son un tipo de variable que permite agrupar diferentes elementos y manejarlos como si fueran una sola variable. En otros términos un arreglo de una lista de variables.</p>
 	<p>Una vez que que un arreglo sea creado, se pueden agregar, editar eliminar sus elementos. Los elementos de un arreglo pueden ser de cualquier tipo y se pueden mezclar, es decir no todos los elementos de un arreglo deben ser del mismo tipo. Los elementos en un arreglo se acceden usando una llave, para esto existen dos tipos de llaves y por lo tanto dos tipos de arreglos en PHP, los numéricos y los asociativos.</p>
 	<p>Los arreglos numéricos son a los que se acceden con números a los datos que guardan, el primer dato tendría como numero el 0, el segundo el 1 y así sucesivamente. Los arreglos asociativos poseen un nombre sobre cada dato y con ese nombre es que se accede al dato especifico.</p>
+	<h2>Crear un arreglo</h2>
+	<p>Para crear un arreglo se hace uso de la función array(). Esta puede tomar de cero a varios parámetros y retorna un arreglo el cual es asignado a una variable con el operado de igualación =. Si la función recibe parámetros, entonces el arreglo será creado con estos valores. Los arreglos en PHP cambian su tamaño dinámicamente dependiendo la cantidad de datos que tengan, por lo tanto no es necesario establecer un tamaño al momento de su creación como en otros lenguaje.</p>
+	<pre>
+		$arreglo = array();
+	</pre>
 	<h2>Arrays numericos</h2>
 	<?php 
 	$arreglo = array('Uno', 'dos', 'tres','cuatro');
